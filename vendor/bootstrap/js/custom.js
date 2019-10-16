57e6f31e5014b99b5e721968ec8cd8e9f60aa623
linettemanuel <38947725+linettemanuel@users.noreply.github.com>
2019-10-16 12:48:16 +0200

    add sliding for option btns

--- vendor/bootstrap/js/custom.js
+++ vendor/bootstrap/js/custom.js
@@ -1,11 +1,33 @@
-var btnContainer = document.querySelector('.navbar-nav');
+let btnContainer = document.querySelector('.navbar-nav');
+let btns = btnContainer.querySelectorAll('.nav-item');
+let arrPosX = [0, 0, 0];
 
-var btns = btnContainer.querySelectorAll('.nav-item');
-
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
+    let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
+
+let moveBtns = document.querySelectorAll(".move-btn");
+
+moveBtns.forEach(moveBtn => {
+    moveBtn.onclick = () => {
+        let optionSlider = moveBtn.parentElement.parentElement.children[1];
+        let options = optionSlider.children[0];
+        let key = options.id - 1;
+        let posX = arrPosX[key];
+        console.log(key);
+        options.style.transition = "1s transform ease";
+        if(moveBtn.classList.contains("btn-front")) {
+            posX -= 9
+            options.style.transform = `translateX(${posX}vw)`;
+            arrPosX[key] = posX;
+        } else {
+            posX += 9
+            options.style.transform = `translateX(${posX}vw)`;
+            arrPosX[key] = posX;
+        }
+    }
+})
