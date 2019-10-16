4a0094db02cf71f366d22fd913d84eb7e84a98ee
linettemanuel <38947725+linettemanuel@users.noreply.github.com>
2019-10-16 11:50:52 +0200

    add custom.js file - change active link in menu

---
+++ vendor/bootstrap/js/custom.js
@@ -0,0 +1,11 @@
+var btnContainer = document.querySelector('.navbar-nav');
+
+var btns = btnContainer.querySelectorAll('.nav-item');
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
